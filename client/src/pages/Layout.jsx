4edbb4190c7260e9def81729db050492f2e4b3c9
Om Patil <ompatilseetara@gmail.com>
2026-01-29 20:25:56 +0530

    Navbar,footer,login upadted

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const Layout=()=>{
     return (
         <div>
-            <h1>Layout Page</h1>
-            <div>
+            <div className='min-h-screen bg-gray-50'>
                 {/* Common layout components like Header, Footer can go here */}
+                <Navbar/>
                 <Outlet/>
             </div>
         </div>
