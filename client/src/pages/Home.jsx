5e05b474affbce44907e78d5ff7f8229c1cfe3e6
Om Patil <ompatilseetara@gmail.com>
2026-01-29 18:22:24 +0530

    features,title, testimonials, footers componenets added to  home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Banner from '../components/home/banner';
 import Hero from '../components/home/Hero';
-import Features from '../components/home/Features'
+import Features from '../components/home/Features';
+import Testimonial from '../components/home/Testimonials';
+import CallToAction from '../components/home/CallToAction';
+import Footer from '../components/home/Footer'
+
 const Home = () => {
     return (
         <div>
             <Banner />
             <Hero/>
             <Features/>
+            <Testimonial/>
+            <CallToAction/>
+            <Footer/>
         </div>
     )
 }
